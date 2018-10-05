76604cc004a8faef584d8a4399120b0d5afc8bf2
USP <USP@C303-13>
2018-10-04 21:58:36 -0500

    Segunda Prueba

--- Prueba.php
+++ Prueba.php
@@ -1,8 +1,10 @@
-<?php
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+</html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
