b79a10b69855cab969f7f76920d450ea65c06702
PC <PC@10.15.11.23>
2018-10-04 22:53:16 -0500

    Lista Prueba

---
+++ Lista.php
@@ -0,0 +1,13 @@
+
+<html>
+ <head>
+  <title>Lista</title>
+ </head>
+ <body>
+     
+     <h1>Lista de #s</h1>
+     <!--<?php echo $_POST["cantidad"]; ?><br>-->
+     <?php for($i=0;$i<=$_POST["cantidad"];$i++)
+        echo "Numero:".$i."<br>"?>
+ </body>
+</html>
--- Prueba.php
+++ Prueba.php
@@ -3,7 +3,7 @@
   <title>Actividad</title>
  </head>
  <body>
-<form action="accion.php" method="post">
+<form action="Lista.php" method="post">
     <p>Cantidad: <input type="text" name="cantidad" placeholder="Ingresar Cantidad"/></p>
  <p><input type="submit" name="Mostrar" value="Mostrar" /></p>
 </form>
